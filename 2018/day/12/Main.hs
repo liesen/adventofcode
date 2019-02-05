469ea7cff35d6e8e6d9dc42cc87232441dbd1c1a
Johan Liesén <johan@liesen.se>
2019-02-05 12:53:07 +0100

    Day 12 2/2

--- 2018/day/12/Main.hs
+++ 2018/day/12/Main.hs
@@ -1,47 +1,82 @@
-import Control.Applicative
-import Data.List
+{-# LANGUAGE BangPatterns #-}
+import Data.Bits
 import Data.Set (Set)
 import qualified Data.Set as Set
+import Data.Word
 import Text.ParserCombinators.ReadP
-import Text.Printf
+import System.IO
+
 
 data Pots = Pots
-    { pots :: String
-    , index :: Int
-    , rules :: [(String, Char)]
-    } deriving (Eq, Ord, Show)
+    { state :: Set Int  -- Pot indices
+    , minIx :: !Int  -- Smallest index (could use Set.findMin)
+    , maxIx :: !Int  -- Largest index (Set.findMax)
+    } deriving (Show)
+
+instance Monoid Pots where
+    mempty = Pots mempty 0 0 
+    mappend (Pots s0 m0 n0) (Pots s1 m1 n1) =
+        Pots (s0 `mappend` s1) (min m0 m1) (max n0 n1)
+
+newtype Rules = Rules (Set Word8)
+
+pot '#' = True
+pot '.' = False
+
+checksumBy f = foldl setBit 0 . map fst . filter (f . snd) . zip [0..]
 
 parse = do
-    s0 <- string "initial state: " *> manyTill get (char '\n')
+    s <- string "initial state: " *> manyTill get (char '\n')
     _ <- char '\n'
-    rules <- sepBy (liftA3 (\r _ y -> (r, y)) (count 5 get) (string " => ") get) (char '\n')
+    r <- sepBy parseRule (char '\n')
     _ <- char '\n'
     eof
-    return $ Pots s0 0 rules
-
-showPots (Pots s i _) = 
-    let (xs, y:ys) = splitAt (-i) s
-    in "(" ++ show i ++ ") " ++ reverse (take 3 (reverse xs)) ++ "[" ++ [y] ++ "]" ++ ys
-
-score (Pots s i _) =
-    sum $ map fst $ filter ((== '#') . snd) $ zip [i..] s
-
-step (Pots s i rules) =
-    let s' = map go $ map (take 5) $ filter ((>= 5) . length) $ tails ("....." ++ s ++ ".....")
-    in Pots s' (i - 3) rules
+    let initialState = Set.fromAscList $ map fst $ filter (pot . snd) $ zip [0..] s
+        pots = Pots initialState (minimum initialState) (maximum initialState)
+        rules = Rules $ Set.fromList $ concatMap ruleBits r
+    return (pots, rules)
   where
-    go xs = case lookup xs rules of
-                Nothing -> '.'
-                Just y  -> y
+    parseRule = do
+        rule <- count 5 get
+        _ <- string " => "
+        y <- get
+        return (rule, y)
 
-clean (Pots s i rules) =
-    let s' = reverse (dropWhile (== '.') (reverse s))
-        a = length (takeWhile (== '.') s')
-    in Pots (drop a s') (i + a) rules
+    ruleBits (rule, y)
+        | pot y     = return $ checksumBy pot rule
+        | otherwise = fail "irrelevant rule"
+
+step :: Rules -> Pots -> Pots
+step (Rules rules) (Pots s m n) =
+    foldr (\i pots@(Pots s' m' n') ->
+               if test s rules i
+                   then insert i pots
+                   else pots)
+        mempty
+        [m - 2..n + 2]
+  where
+    test s rules i = let b = checksumBy (flip Set.member s) [i - 2..i + 2]
+                     in Set.member b rules
+    insert i (Pots s m n) = Pots (Set.insert i s) (min m i) (max n i)
+
+score (Pots s _ _) = sum $ Set.toList s
 
 main = do
     input <- readFile "input.txt"
-    let [(s0, "")] = readP_to_S parse input
+    let [((s0, rules), [])] = readP_to_S parse input
 
     -- Part 1
-    print $ score $ iterate step s0 !! 20
+    print $ score $ iterate (step rules) s0 !! 1000
+
+    -- Part 2
+    -- This will reveal the pattern: score = generation * 40 + 1684
+    {-
+    hSetBuffering stdout NoBuffering
+    mapM_ (\(i, p) -> if i `mod` 1000 == 0
+                          then do putStr $ show i
+                                  putChar ','
+                                  putStrLn $ show (score p)
+                          else return ())
+          (zip [0..] (iterate (step rules) s0))
+    -}
+    print $ 50000000000 * 40 + 1682
