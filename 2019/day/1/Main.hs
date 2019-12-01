54a578ca6a3945b506d4178eb2e8075c269f0240
Johan Liesén <johan@liesen.se>
2019-12-01 13:43:50 +0100

    Use foldMap

--- 2019/day/1/Main.hs
+++ 2019/day/1/Main.hs
@@ -1,3 +1,5 @@
+import Data.Monoid
+
 fuel x = (x `div` 3) - 2
 
 fuelfuel = sum . takeWhile (> 0) . tail . iterate fuel
@@ -6,7 +8,7 @@ main = do
     input <- readFile "input.txt"
     
     -- Part 1
-    print $ sum . map (fuel . read) $ lines input
+    print $ getSum . foldMap (Sum . fuel . read) $ lines input
 
     -- Part 2
-    print $ sum . map (fuelfuel . read) . lines $ input
+    print $ getSum . foldMap (Sum . fuelfuel . read) $ lines input
