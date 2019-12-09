084fb9ae1072a879717821508065923ad8f7fd5a
Johan Liesén <johan@liesen.se>
2019-12-09 23:56:28 +0100

    2019-12-07 2/2

---
+++ 2019/day/7/intcode.py
@@ -0,0 +1,73 @@
+from collections import deque
+
+class Intcode:
+    def __init__(self, prog, inputs=[]):
+        self.prog = prog
+        self.pc = 0
+        self.mem = list(prog)
+        self.halt = False
+        self.inputs = deque(inputs)
+        self.outputs = deque()
+
+    def reset(self):
+        self.pc = 0
+        self.halt = False
+        self.mem = list(self.prog)
+
+    def run(self, inputs=[]):
+        self.inputs.extend(inputs)
+
+        while not (self.halt or self.need_input):
+            self.step()
+
+    def step(self):
+        def param(i):
+            mode = (self.mem[self.pc] // (10 * 10**i)) % 10
+
+            if mode == 0:
+                return self.mem[self.mem[self.pc + i]]
+            elif mode == 1:
+                return self.mem[self.pc + i]
+
+            raise Exception("bad parameter mode")
+
+        if not self.halt:
+            opcode = self.mem[self.pc] % 100
+
+            if opcode == 1:
+                self.mem[self.mem[self.pc + 3]] = param(1) + param(2)
+                self.pc += 4
+            elif opcode == 2:
+                self.mem[self.mem[self.pc + 3]] = param(1) * param(2)
+                self.pc += 4
+            elif opcode == 3:
+                if len(self.inputs) > 0:
+                    self.mem[self.mem[self.pc + 1]] = self.inputs.popleft()
+                    self.pc += 2
+            elif opcode == 4:
+                self.outputs.append(param(1))
+                self.pc += 2
+            elif opcode == 5:
+                if param(1) == 0:
+                    self.pc += 3
+                else:
+                    self.pc = param(2)
+            elif opcode == 6:
+                if param(1) == 0:
+                    self.pc = param(2)
+                else:
+                    self.pc += 3
+            elif opcode == 7:
+                self.mem[self.mem[self.pc + 3]] = 1 if param(1) < param(2) else 0
+                self.pc += 4
+            elif opcode == 8:
+                self.mem[self.mem[self.pc + 3]] = 1 if param(1) == param(2) else 0
+                self.pc += 4
+            elif opcode == 99:
+                self.halt = True
+            else:
+                raise Exception(f'unknown opcode: {opcode} at pc = {pc}')
+
+    @property
+    def need_input(self):
+        return self.mem[self.pc] % 100 == 3 and len(self.inputs) == 0
--- 2019/day/7/main.py
+++ 2019/day/7/main.py
@@ -1,87 +1,23 @@
 from collections import deque, namedtuple
 from itertools import permutations
+from functools import reduce
+from intcode import Intcode
 
 with open('input.txt') as fp:
     prog = [int(i) for line in fp for i in line.split(',')]
 
-# prog = [3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0]
-# prog = [3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0]
-
 # Part 1
-max_thruster_signal = 0
+def feedback(input_signal, amp):
+    amp.run([input_signal])
+    return amp.outputs.pop()
 
-def run(prog, phase_settings):
-    input_signal = 0
+print(max(reduce(feedback, [Intcode(prog, [phase]) for phase in phases], 0) for phases in permutations(range(5))))
 
-    for phase_setting in phase_settings:
-        pc = 0
-        halt = False
-        mem = list(prog)
-        inp = iter([phase_setting, input_signal])
-        outp = deque()
-
-        def param(i):
-            mode = (mem[pc] // (10 * 10**i)) % 10
-
-            if mode == 0:
-                return mem[mem[pc + i]]
-            elif mode == 1:
-                return mem[pc + i]
-
-            raise Exception("bad parameter mode")
-
-        while not halt:
-            opcode = mem[pc] % 100
-            # print(f'pc={pc}, opcode={opcode}, mem={mem}')
-
-            if opcode == 1:
-                mem[mem[pc + 3]] = param(1) + param(2)
-                pc += 4
-            elif opcode == 2:
-                mem[mem[pc + 3]] = param(1) * param(2)
-                pc += 4
-            elif opcode == 3:
-                mem[mem[pc + 1]] = next(inp)
-                pc += 2
-            elif opcode == 4:
-                outp.append(param(1))
-                pc += 2
-            elif opcode == 5:
-                if param(1) == 0:
-                    pc += 3
-                else:
-                    pc = param(2)
-            elif opcode == 6:
-                if param(1) == 0:
-                    pc = param(2)
-                else:
-                    pc += 3
-            elif opcode == 7:
-                mem[mem[pc + 3]] = 1 if param(1) < param(2) else 0
-                pc += 4
-            elif opcode == 8:
-                mem[mem[pc + 3]] = 1 if param(1) == param(2) else 0
-                pc += 4
-            elif opcode == 99:
-                halt = True
-            else:
-                raise Exception(f'unknown opcode: {opcode} at pc = {pc}')
-
-        input_signal = output_signal = outp.pop()
+# Part 2
+def feedback_loop(input_signal, amps):
+    while not all(amp.halt for amp in amps):
+        input_signal = output_signal = reduce(feedback, amps, input_signal)
 
     return output_signal
 
-
-# Part 1
-max_thruster_signal = 0
-
-for phases in permutations(range(5)):
-    thruster_signal = run(prog, phases)
-
-    if thruster_signal > max_thruster_signal:
-        max_thruster_signal = thruster_signal
-
-print(max_thruster_signal)
-
-# Part 2
-prog = [3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5]
+print(max(feedback_loop(0, [Intcode(prog, [phase]) for phase in phases]) for phases in permutations(range(5, 10))))
