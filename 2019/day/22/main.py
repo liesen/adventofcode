f4b7ca2aa8c22f6d1b752d61ebd97eeaa1596a12
Johan Liesén <johan@liesen.se>
2019-12-22 23:28:54 +0100

    Fix typo in 2019-12-22

--- 2019/day/22/main.py
+++ 2019/day/22/main.py
@@ -4,15 +4,20 @@ with open('input.txt') as fp:
 # Part 1
 N = 10007
 x = 2019
+y = 1498
 
-for ln in enumerate(S.splitlines()):
+for ln in S.splitlines():
     if ln == 'deal into new stack':
         x = N - x - 1
+        y = N - (N - y - 1)
     elif ln.startswith('deal with increment'):
         i = int(ln[len('deal with increment '):])
         x = (i * x) % N
+        y = N - ((i * y) % N)
     elif ln.startswith('cut'):
         i = int(ln[len('cut '):])
         x = (N + x - i) % N
+        y = N - ((N + y - i) % N)
 
 print(x)
+print(y)
