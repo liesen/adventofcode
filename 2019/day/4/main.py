435700121ccfed33c032bb382b01931a94f39b92
Johan Liesén <johan@liesen.se>
2019-12-27 17:02:48 +0100

    Fixup 2019-12-04

--- 2019/day/4/main.py
+++ 2019/day/4/main.py
@@ -1,15 +1,13 @@
-#%%
 from itertools import groupby
 lo, hi = map(int, "146810-612564".split('-', 2))
 
-#%%
 # Part 1
-%time sum(any([len(list(g)) >= 2 for k, g in groupby(s, lambda x: x)]) \
+print(sum(any([len(list(g)) >= 2 for k, g in groupby(s, lambda x: x)]) \
           for s in map(str, range(lo, hi + 1)) \
-          if all(map(lambda x: x[0] <= x[1], zip(s, s[1:]))))
+          if all(map(lambda x: x[0] <= x[1], zip(s, s[1:])))))
  
 # Part 2
-#%%
-%time sum(any([len(list(g)) == 2 for k, g in groupby(s, lambda x: x)]) \
+print(sum(any([len(list(g)) == 2 for k, g in groupby(s, lambda x: x)]) \
           for s in map(str, range(lo, hi + 1)) \
-          if all(map(lambda x: x[0] <= x[1], zip(s, s[1:]))))
+          if all(map(lambda x: x[0] <= x[1], zip(s, s[1:])))))
+
