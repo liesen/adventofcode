2bdcbf1f856440e1c8478767e026e9e6f298c5a5
Johan Liesén <johan@liesen.se>
2020-01-07 20:04:36 +0100

    2019-12-21 2/2

--- 2019/day/21/main.py
+++ 2019/day/21/main.py
@@ -3,8 +3,9 @@ from itertools import chain
 
 
 with open('input.txt') as fp:
-    prog = Intcode([int(x) for x in fp.read().split(',')])
+    image = [int(x) for x in fp.read().split(',')]
 
+# Part 1
 # Idea: the jump is four tiles wide, so if there's
 # ground four tiles away (D) and not ground anywhere
 # in between (~A | ~B | ~C), then jump
@@ -17,6 +18,36 @@ AND D J
 WALK
 '''
 
+prog = Intcode(image)
+prog.runs(scriptcode)
+
+for z in prog.outputs:
+    if z > 256:
+        ans = z
+        print(ans)
+        break
+
+    print(chr(z), end='')
+
+# Part 2
+# ABCDEFGHI
+#    ^   ^
+scriptcode = '''NOT A J
+NOT B T
+OR T J
+NOT C T
+OR T J
+AND D J
+NOT I T
+NOT T T
+OR F T
+AND E T
+OR H T
+AND T J
+RUN
+'''
+
+prog = Intcode(image)
 prog.runs(scriptcode)
 
 for z in prog.outputs:
