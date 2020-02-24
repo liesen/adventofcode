a8f5c7c4cc124eaa852e4b0623c4ff7311cd469c
Johan Liesén <johan@liesen.se>
2020-02-24 22:03:13 +0100

    2015-12-17 2/2

--- 2015/day/17/Main.hs
+++ 2015/day/17/Main.hs
@@ -1,10 +1,18 @@
-pack 0 _      = 1
-pack n []     = 0
-pack n (x:xs) = pack n xs + if x <= n then pack (n - x) xs else 0
+import Data.List
+import Data.Ord
+import Data.Function (on)
 
+pack :: Int -> [Int] -> [[Int]]
+pack 0 _      = [[]]
+pack n []     = []
+pack n (x:xs) = pack n xs ++ if x <= n then map (x :) (pack (n - x) xs) else []
 
 main = do
     input <- readFile "input.txt"
     let containers = map read (lines input)
 
-    print $ pack 150 containers
+    -- Part 1
+    print $ length $ pack 150 containers
+
+    -- Part 2
+    print $ length $ head $ groupBy ((==) `on` length) $ sortBy (comparing length) $ pack 150 containers
