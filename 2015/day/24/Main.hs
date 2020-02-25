69d77698a4037a8692e13733d8fd73dfdd59799a
Johan Liesén <johan@liesen.se>
2020-02-26 00:17:57 +0100

    2015-12-24 2/2

--- 2015/day/24/Main.hs
+++ 2015/day/24/Main.hs
@@ -1,16 +1,18 @@
-import Data.List (sortBy)
+import Data.List
 import Data.Ord
+import Data.Function (on)
 
-group 0 []     = [[]]
-group _ []     = []
-group n (x:xs) = map (x :) (group (n - x) xs) ++ group n xs
+-- Find set of elements of a list summing to a value
+sumGroups 0 []     = [[]]
+sumGroups _ []     = []
+sumGroups n (x:xs) = map (x :) (sumGroups (n - x) xs) ++ sumGroups n xs
 
 main = do
     input <- readFile "input.txt"
     let packages = map read (lines input)
-        groupSum = sum packages `div` 3
     
     -- Part 1
-    -- Simply find the smallest group of packages (by sorting
-    -- from largest to smallest) summing to {all packages} / 3
-    print $ product $ head $ group groupSum $ sortBy (comparing Down) packages
+    print $ snd $ minimum $ map (\g -> (length g, product g)) $ sumGroups (sum packages `div` 3) packages
+
+    -- Part 2
+    print $ snd $ minimum $ map (\g -> (length g, product g)) $ sumGroups (sum packages `div` 4) packages
