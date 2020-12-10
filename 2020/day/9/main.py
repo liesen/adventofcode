55a5de82b64fbc785a2319263f853d4d56a658c5
Johan Liesén <johan@liesen.se>
2020-12-10 15:29:09 +0100

    2020-12-09 touch up

--- 2020/day/9/main.py
+++ 2020/day/9/main.py
@@ -7,28 +7,25 @@ i = PREAMBLE_LEN + 1
 while i < len(numbers) - PREAMBLE_LEN:
     ans1 = numbers[i]
     preamble = numbers[i - PREAMBLE_LEN:i]
-    ok = False
 
-    for m in preamble:
-        if ans1 - m in preamble:
-            ok = True
-            break
-
-    if not ok:
-        print(ans1)
+    if all(ans1 - m not in preamble for m in preamble):
         break
 
     i += 1
 
+print(ans1)
+
 # Part 2
-for i in range(len(numbers)):
-    s = 0
-    j = i
+acc, lo, hi = 0, 0, 0
 
-    while j < len(numbers) and s < ans1:
-        s += numbers[j]
-        j += 1
+while acc != ans1:
+    if acc < ans1:
+        acc += numbers[hi]
+        hi += 1
+    elif acc > ans1:
+        acc -= numbers[lo]
+        lo += 1
 
-    if s == ans1:
-        print(max(numbers[i:j]) + min(numbers[i:j]))
-        break
+assert acc == ans1
+ans2 = min(numbers[lo:hi]) + max(numbers[lo:hi])
+print(ans2)
