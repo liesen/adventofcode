96ae6b56e5b315ee88bf9cec9d9ddb5cd6618440
Johan Liesén <johan@liesen.se>
2021-12-09 13:21:56 +0100

    2021-12-08 permutations

--- 2021/day/8/Main.hs
+++ 2021/day/8/Main.hs
@@ -19,7 +19,32 @@ parseProblem = Problem <$> parseSegments <*> (string " | " *> parseSegments)
 parseProblems :: ReadP [Problem]
 parseProblems = sepBy1 parseProblem (char '\n')
 
-solve2 (Problem inputs outputs) =
+ex = Problem inputs outputs
+  where
+    inputs = words "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab"
+    outputs = words "cdfeb fcadb cdfeb cdbaf"
+
+-- Find a lookup table that produces valid numbers for every
+-- input string by trying all permutations. Assumes that there is only one
+-- such permutation.
+brute :: Problem -> Maybe Int
+brute (Problem inputs outputs) = listToMaybe $ do
+    p <- permutations "abcdefg"
+    let table = zip p "abcdefg"
+        decode = mapM (`lookup` table)
+        encode = flip lookup numbersTable . sort
+
+    case mapM (decode >=> encode) inputs of
+        Nothing -> fail "not a valid permutation for inputs"
+        Just _ -> case mapM (decode >=> encode) outputs of
+                      Nothing -> fail "not a valid permutation for outputs"
+                      Just xs -> return $ read $ map intToDigit xs
+
+-- Analytically find a lookup table that is able to unique translate
+-- inputs to valid numbers. Assumes that there are numbers in the 
+-- input that unique determines such a table (ones, fours, sevens, etc).
+clever :: Problem -> Maybe Int
+clever (Problem inputs outputs) =
     let [a] = foldr1 union (filter ((== 3) . length) inputs) \\ foldr1 union (filter ((== 2) . length) inputs)
         adg = foldr1 intersect (filter ((== 5) . length) inputs)
         dg = adg \\ [a]
@@ -37,18 +62,19 @@ solve2 (Problem inputs outputs) =
     in read <$> map intToDigit <$> mapM (decode >=> encode) outputs
   where
     encode = flip lookup numbersTable . sort
-    numbersTable = [
-            ("abcefg", 0),
-            ("cf", 1),
-            ("acdeg", 2),
-            ("acdfg", 3),
-            ("bcdf", 4),
-            ("abdfg", 5),
-            ("abdefg", 6),
-            ("acf", 7),
-            ("abcdefg", 8),
-            ("abcdfg", 9)
-        ]
+
+numbersTable = [
+        ("abcefg", 0),
+        ("cf", 1),
+        ("acdeg", 2),
+        ("acdfg", 3),
+        ("bcdf", 4),
+        ("abdfg", 5),
+        ("abdefg", 6),
+        ("acf", 7),
+        ("abcdefg", 8),
+        ("abcdfg", 9)
+    ]
 
 main = do
     input <- readFile "input.txt"
@@ -58,4 +84,4 @@ main = do
     print $ length $ filter (`elem` [2, 3, 4, 7]) $ concatMap (map length . outputs) problems
 
     -- Part 2
-    print $ fromJust $ sum <$> mapM solve2 problems
+    print $ fromJust $ sum <$> mapM clever problems
