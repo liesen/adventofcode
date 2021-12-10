6207b8c15605724578a9a5c33262b6c58ba969bf
Johan Liesén <johan@liesen.se>
2021-12-10 12:14:15 +0100

    2021-12-10 2/2

--- 2021/day/10/Main.hs
+++ 2021/day/10/Main.hs
@@ -1,11 +1,10 @@
 {-# LANGUAGE LambdaCase #-}
-import Text.ParserCombinators.ReadP
 import Data.List
 
 data Result
     = Ok
     | Incomplete String
-    | CorruptChar Char
+    | Corrupt Char
     deriving (Show)
         
 invert = \case
@@ -20,30 +19,40 @@ invert = \case
 
 open = flip elem "([{<"
 
-close = flip elem ")]}>"
-
 parse = p []
+    where
+        p []       []     = Ok
+        p []       (y:ys)
+            | open y = p [y] ys
+            | otherwise = Corrupt y
+        p (x:xs)   []     = Incomplete (x:xs)
+        p (x:xs)   (y:ys)
+            | open y = p (y:x:xs) ys
+            | y == invert x = p xs ys
+            -- Expected (invert x) but found y instead
+            | otherwise = Corrupt y
 
-p []       []     = Ok
-p []       (y:ys)
-    | close y = CorruptChar y
-    | otherwise = p [y] ys
-p (x:xs)   []     = Incomplete (x:xs)
-p (x:xs)   (y:ys)
-    | open y = p (y:x:xs) ys
-    | close y && open x && y == invert x = p xs ys
-    | otherwise =
-        -- Expected (invert x) but found y instead
-        CorruptChar y
+score1 (Corrupt ')') = 3
+score1 (Corrupt ']') = 57
+score1 (Corrupt '}') = 1197
+score1 (Corrupt '>') = 25137
+score1 _ = 0
 
-score (CorruptChar ')') = 3
-score (CorruptChar ']') = 57
-score (CorruptChar '}') = 1197
-score (CorruptChar '>') = 25137
-score _ = 0
+score2 (Incomplete rest) = foldl' f 0 rest
+  where
+    f acc '(' = acc * 5 + 1
+    f acc '[' = acc * 5 + 2
+    f acc '{' = acc * 5 + 3
+    f acc '<' = acc * 5 + 4
+score2 _ = 0
 
 main = do
      input <- readFile "input.txt"
 
      -- Part 1
-     print $ sum $ map (score . parse) (lines input)
+     print $ sum $ map (score1 . parse) (lines input)
+
+     -- Part 2
+     let xs = [score2 r | r@(Incomplete _) <- parse <$> lines input]
+         n = length xs
+     print $ head $ drop (n `div` 2) $ sort xs
