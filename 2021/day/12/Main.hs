738d184447beb6d6cc4a03baf126644575c11b6b
Johan Liesén <johan@liesen.se>
2021-12-13 11:57:06 +0100

    2021-12-12 dry

--- 2021/day/12/Main.hs
+++ 2021/day/12/Main.hs
@@ -21,8 +21,6 @@ count2 caves seen1 _       []         = 0
 count2 caves seen1 _       ("end":xs) = 1
 count2 caves seen1 Nothing (x:q1)  -- Able to double-visit any small cave
     | Set.member x seen1 = sum $ map (count2 caves seen1 (Just x) . (:q1)) q2  -- Double-visit x
-    | all isUpper x      = sum $ map (count2 caves seen1 Nothing . (:q1)) q2
-    | all isLower x      = sum $ map (count2 caves (Set.insert x seen1) Nothing . (:q1)) q2
     where
         q2 = neighbors caves x
 count2 caves seen1 seen2   (x:q1)  -- Already double-visited some cave
