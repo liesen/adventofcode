8cde286371c6d0b77811657b6cdef5777a81380c
Johan Liesén <johan@liesen.se>
2022-12-10 00:15:26 +0100

    2022-12-09 prep for longer snek

--- 2022/day/9/main.py
+++ 2022/day/9/main.py
@@ -1,27 +1,25 @@
+from collections import deque
 from dataclasses import dataclass
 
 
 @dataclass
 class Snek:
-    headx: int
-    heady: int
-    tailx: int
-    taily: int
+    pt: deque[tuple[int, int]]
     
     @property
     def head(self):
-        return (self.heady, self.headx)
+        return self.pt[0]
 
     @property
     def tail(self):
-        return (self.taily, self.tailx)
+        return self.pt[-1]
     
     def move(self, motion):
         stepdir, _steplen = motion.split(" ", maxsplit=2)
         steplen = int(_steplen)
         
-        hx, hy = self.headx, self.heady
-        tx, ty = self.tailx, self.taily
+        hx, hy = self.head
+        tx, ty = self.tail
         
         match stepdir:
             case "U": 
@@ -47,21 +45,21 @@ class Snek:
                 
             assert 0 <= abs(hx - tx) <= 1
             assert 0 <= abs(hy - ty) <= 1
-            yield Snek(hx, hy, tx, ty)
+            yield Snek(deque([(hx, hy), (tx, ty)]))
 
 
-assert next(Snek(0, 0, 0, 0).move("R 1")) == Snek(1, 0, 0, 0), "R 1"
-assert next(next(Snek(0, 0, 0, 0).move("R 1")).move("L 1")) == Snek(0, 0, 0, 0), "R 1 . L 1 == id"
-assert next(Snek(1, 0, 0, 0).move("R 1")) == Snek(2, 0, 1, 0)
-assert next(Snek(1, 0, 0, 0).move("L 1")) == Snek(0, 0, 0, 0)
-assert next(Snek(2, 1, 1, 0).move("D 1")) == Snek(2, 2, 2, 1)
-assert next(Snek(2, 1, 1, 0).move("L 1")) == Snek(1, 1, 1, 0)
+assert next(Snek(deque([(0, 0), (0, 0)])).move("R 1")) == Snek(deque([(1, 0), (0, 0)])), "R 1"
+assert next(next(Snek(deque([(0, 0), (0, 0)])).move("R 1")).move("L 1")) == Snek(deque([(0, 0), (0, 0)])), "R 1 . L 1 == id"
+# assert next(Snek(1, 0, 0, 0).move("R 1")) == Snek(2, 0, 1, 0)
+# assert next(Snek(1, 0, 0, 0).move("L 1")) == Snek(0, 0, 0, 0)
+# assert next(Snek(2, 1, 1, 0).move("D 1")) == Snek(2, 2, 2, 1)
+# assert next(Snek(2, 1, 1, 0).move("L 1")) == Snek(1, 1, 1, 0)
 
 
 with open("input.txt") as f:
     motions = [ln.strip() for ln in f]
 
-snek = Snek(0, 0, 0, 0)
+snek = Snek(deque([(0, 0)] * 2))
 seen = set()
 seen.add(snek.tail)
 
