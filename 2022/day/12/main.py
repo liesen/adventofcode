6396f8e05a5e7bd915512e068a2fcfc8ccc497d7
Johan Liesén <johan@liesen.se>
2022-12-12 14:03:30 +0100

    2022-12-12 astar

--- 2022/day/12/main.py
+++ 2022/day/12/main.py
@@ -1,4 +1,4 @@
-from collections import deque
+import heapq
 
 
 with open("input.txt") as f:
@@ -7,46 +7,57 @@ with open("input.txt") as f:
         for r, ln in enumerate(f)
         for c, x in enumerate(ln.rstrip())
     }
-    
+
     S = next(p for p, x in G.items() if x == 'S')
     G[S] = 'a'
     E = next(p for p, x in G.items() if x == 'E')
     G[E] = 'z'
-    
+
     G = {p: ord(x) - ord('a') for p, x in G.items()}
 
-def find_path_length(G, S, E):
-    queue = deque([(S, 0)])
-    seen = set()
-
-    while queue:
-        p, n = queue.popleft()
-
-        if p in seen:
-            continue
-
-        seen.add(p)
-        
-        if p == E:
-            return n
-        
-        for dr, dc in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-            r, c = p
-            q = (r + dr, c + dc)
-
-            if q in G and G[q] - G[p] <= 1: 
-                queue.append((q, n + 1))
-
 # Part 1
-print(find_path_length(G, S, E))
+queue = [(0, S)]
+seen = set()
+
+while queue:
+    n, p = heapq.heappop(queue)
+
+    if p == E:
+        print(n)
+        break
+
+    if p in seen:
+        continue
+
+    seen.add(p)
+
+    for dr, dc in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+        r, c = p
+        q = (r + dr, c + dc)
+
+        if q in G and G[q] - G[p] <= 1:
+            heapq.heappush(queue, (n + 1, q))
+
 
 # Part 2
-print(
-    min(
-        n
-        for s in G
-        if G[s] == 0
-        for n in [find_path_length(G, s, E)]
-        if n is not None
-    )
-)
+queue = [(0, s) for s in G if G[s] == 0]
+seen = set()
+
+while queue:
+    n, p = heapq.heappop(queue)
+
+    if p == E:
+        print(n)
+        break
+
+    if p in seen:
+        continue
+
+    seen.add(p)
+
+    for dr, dc in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+        r, c = p
+        q = (r + dr, c + dc)
+
+        if q in G and G[q] - G[p] <= 1:
+            heapq.heappush(queue, (n + 1, q))
