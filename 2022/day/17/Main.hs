a74620e84a63aca743fca76b571fd2b1a77562c6
Johan Liesén <johan@liesen.se>
2023-01-02 01:20:25 +0100

    2022-12-17 refactor: add names for shapes

--- 2022/day/17/Main.hs
+++ 2022/day/17/Main.hs
@@ -6,39 +6,50 @@ import Data.Foldable
 import Control.Arrow
 import Data.Maybe
 
+type Rocks = Set (Int, Int)
 
-type Shape = Set (Int, Int)
+data RockId = HBar | Plus | Edge | VBar | Square
+  deriving (Eq, Ord, Show)
+
+data Shape = Shape RockId Rocks
 
 hbar, plus, vbar, edge, square :: Shape
-hbar = Set.fromList [(0, x) | x <- [0..3]]
-plus = Set.fromList [(0, 1), (1, 0), (1, 1), (1, 2), (2, 1)]
-vbar = Set.fromList [(y, 0) | y <- [0..3]]
-edge = Set.fromList [(0, 2), (1, 2), (2, 2), (0, 1), (0, 0)]
-square = Set.fromList [(x, y) | y <- [0, 1], x <- [0, 1]]
+hbar = Shape HBar $ Set.fromList [(0, x) | x <- [0..3]]
+plus = Shape Plus $ Set.fromList [(0, 1), (1, 0), (1, 1), (1, 2), (2, 1)]
+edge = Shape Edge $ Set.fromList [(0, 2), (1, 2), (2, 2), (0, 1), (0, 0)]
+vbar = Shape VBar $ Set.fromList [(y, 0) | y <- [0..3]]
+square = Shape Square $ Set.fromList [(x, y) | y <- [0, 1], x <- [0, 1]]
 
-shapes = [hbar, plus, edge, vbar, square]
+shapes = [
+        hbar,
+        plus,
+        edge,
+        vbar,
+        square
+    ]
 
-moveLeft, moveRight :: Shape -> Shape -> Shape
+moveLeft, moveRight :: Rocks -> Shape -> Shape
 moveLeft rocks shape = hmove rocks shape (-1)
 moveRight rocks shape = hmove rocks shape 1
 
-hmove rocks shape dx
-    | not (Set.disjoint rocks shape') = shape  -- ??? move horizontally into rocks
+hmove rocks shape@(Shape name s) dx
+    | not (Set.disjoint rocks s') = shape  -- ??? move horizontally into rocks
     | xmin < 0 || xmax > 6 = shape
-    | otherwise = shape'
-  where shape' = Set.map (second (+ dx)) shape
-        xmin = minimum (Set.map snd shape')
-        xmax = maximum (Set.map snd shape')
+    | otherwise = Shape name s'
+  where s' = Set.map (second (+ dx)) s
+        xmin = minimum (Set.map snd s')
+        xmax = maximum (Set.map snd s')
 
-moveDown rocks shape
+moveDown rocks shape@(Shape name s)
     | ymin' == 0 = fail "hit the floor"
-    | not (Set.disjoint shape' rocks) = fail "hit resting rock"
+    | not (Set.disjoint s' rocks) = fail "hit resting rock"
     | otherwise = pure shape'
   where
-    shape' = Set.map (first (+ (-1))) shape
-    ymin' = minimum (Set.map fst shape')
+    s' = Set.map (first (+ (-1))) s
+    ymin' = minimum (Set.map fst s')
+    shape' = Shape name s'
 
-move rocks shape (x:xs) =
+move rocks shape@(Shape name s) (x:xs) =
     case moveDown rocks shape' of
         Nothing -> (shape', xs)
         Just shape'' -> move rocks shape'' xs
@@ -48,25 +59,28 @@ move rocks shape (x:xs) =
     shape' = hmove rocks shape
 
 -- Adjust starting position
-adjust rocks shape = Set.map (((+ yfloor) . (+ ymin) . (+ 3) . (+ 1)) *** (+ 2)) shape
+adjust rocks shape@(Shape name s) =
+    Shape name $ Set.map (((+ yfloor) . (+ ymin) . (+ 3) . (+ 1)) *** (+ 2)) s
   where
     yfloor = fromMaybe 0 (Set.lookupMax (Set.map fst rocks))
-    ymin = Set.findMin (Set.map fst shape)
+    ymin = Set.findMin (Set.map fst s)
 
-run program = go mempty (cycle program) (cycle shapes)
+r0 = Set.fromList [(0, x) | x <- [0..6]]
 
-go rocks program []     = []
-go rocks program (s:ss) =
-    let (s', program') = move rocks (adjust rocks s) program
-        rocks' = rocks <> s'
-    in rocks : go rocks' program' ss
+data State = State Rocks (Maybe RockId) String [Shape]
+
+run program = iterate f (State r0 Nothing (cycle program) (cycle shapes))
+  where
+    f (State rocks _ p (s:ss)) = State (rocks <> s') (Just x) p' ss
+      where
+        (Shape x s', p') = move rocks (adjust rocks s) p
 
 main = do
     input <- readFile "input.txt"
     let program = head (lines input)
 
     -- Part 2
-    let tower1 = run program !! 2022
-        ans1 = maximum (Set.map fst tower1)
+    let State rocks1 _ _ _ = run program !! 2022
+        ans1 = maximum (Set.map fst rocks1)
     print ans1
 
