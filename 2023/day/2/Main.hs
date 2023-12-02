0837c6690f73bac257804b799bbcd3df8adf12bc
liesen <johan@liesen.se>
2023-12-02 14:14:23 +0100

    2023-12-02 tidy

--- 2023/day/2/Main.hs
+++ 2023/day/2/Main.hs
@@ -10,9 +10,7 @@ data Color = Red | Green | Blue deriving (Eq, Ord, Show)
 
 newtype Round = Round [(Color, Int)] deriving (Show)
 
-data Game = Game Int [Round] deriving (Show)
-
-gameId (Game x _) = x
+data Game = Game {gameId :: Int, rounds :: [Round]} deriving (Show)
 
 pcolor :: ReadP Color
 pcolor = choice [string "red" $> Red, string "green" $> Green, string "blue" $> Blue]
@@ -23,16 +21,21 @@ pcube = flip (,) <$> pnum <*> (char ' ' *> pcolor)
 pnum :: ReadP Int
 pnum = read <$> munch1 isDigit
 
+pround :: ReadP Round
 pround = Round <$> sepBy1 pcube (string ", ")
 
+pgame :: ReadP Game
 pgame = Game <$> (string "Game " *> pnum <* string ": ") <*> sepBy1 pround (string "; ")
 
-valid1 (Game gameId rounds) = all p rounds
+valid1 :: Game -> Bool
+valid1 = all p . rounds
   where
     p (Round xs) = fromMaybe 0 (lookup Red xs) <= 12 && fromMaybe 0 (lookup Green xs) <= 13 && fromMaybe 0 (lookup Blue xs) <= 14
 
+fewest :: Game -> [(Color, Int)]
 fewest (Game _ rounds) = map (\((color, n) : cubes) -> (color, maximum (n : map snd cubes))) $ groupBy ((==) `on` fst) $ sortBy (comparing fst) $ concatMap (\(Round xs) -> xs) rounds
 
+power :: [(Color, Int)] -> Int
 power = product . map snd
 
 main = do
