f43598e062f6ce77aa4903f3bef882b9f6cbdb2e
liesen <johan@liesen.se>
2023-12-09 14:56:49 +0100

    2023-12-09 no view patterns

--- 2023/day/9/Main.hs
+++ 2023/day/9/Main.hs
@@ -1,11 +1,9 @@
-{-# LANGUAGE ViewPatterns #-}
-
 extrapolate xs
   | all (== 0) xs = (0, 0)
   | otherwise = let (front, back) = extrapolate (zipWith (-) (tail xs) xs) in (head xs - front, last xs + back)
 
 main = do
   input <- map (map read . words) . lines <$> readFile "input"
-  let (sum -> ans2, sum -> ans1) = unzip $ map extrapolate input
-  print ans1
-  print ans2
+  let (fronts, backs) = unzip $ map extrapolate input
+  print $ sum backs
+  print $ sum fronts
