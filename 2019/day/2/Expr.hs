2f9386f3df6ab2d83e10622f1d064df5e0f1ea05
liesen <johan@liesen.se>
2024-09-16 00:22:11 +0200

    2019-12-02 tidy

--- 2019/day/2/Expr.hs
+++ 2019/day/2/Expr.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE RecordWildCards #-}
+
 import Control.Monad
 import Control.Monad.Except
 import Data.Array
@@ -12,20 +13,20 @@ import Debug.Trace
 
 -- Expression tree
 data Expr
-    = Lit Int
-    | Verb
-    | Noun
-    | Add Expr Expr
-    | Mul Expr Expr
+  = Lit Int
+  | Verb
+  | Noun
+  | Add Expr Expr
+  | Mul Expr Expr
 
 -- Evaluation
-data Env = Env { verb :: Int, noun :: Int }
+data Env = Env {verb :: Int, noun :: Int}
 
-eval env     (Lit n)  = n
-eval Env{..} Verb = verb
-eval Env{..} Noun = noun
-eval env     (Add a b) = eval env a + eval env b
-eval env     (Mul a b) = eval env a * eval env b
+eval env (Lit n) = n
+eval Env {..} Verb = verb
+eval Env {..} Noun = noun
+eval env (Add a b) = eval env a + eval env b
+eval env (Mul a b) = eval env a * eval env b
 
 showExpr :: Expr -> String
 showExpr (Lit n) = show n
@@ -39,68 +40,71 @@ showFactor (Add a b) = "(" ++ showExpr (Add a b) ++ ")"
 showFactor e = showExpr e
 
 instance Show Expr where
-    show = showExpr
+  show = showExpr
 
 instance Num Expr where
-    fromInteger = Lit . fromInteger
-    (+) = Add
-    (*) = Mul
-    negate = error "not implemented"
-    abs = error "not implemented"
-    signum = error "not implemented"
+  fromInteger = Lit . fromInteger
+  (+) = Add
+  (*) = Mul
+  negate = error "not implemented"
+  abs = error "not implemented"
+  signum = error "not implemented"
 
 -- Intcode memory now stores the value and the expression that was used to
 -- calculate it
 data Intcode = Intcode Int (Array Int (Int, Expr))
-    deriving Show
+  deriving (Show)
 
-data Intstep = Halt
-             | Cont Intcode
+data Intstep
+  = Halt
+  | Cont Intcode
 
 step :: Intcode -> Except String Intstep
 step (Intcode pc mem) =
-    case opcode of
-        99 -> return Halt
-        1 -> 
-            let (m, a) = mem ! fst (mem ! (pc + 1))
-                (n, b) = mem ! fst (mem ! (pc + 2))
-                output = fst (mem ! (pc + 3))
-            in return $ Cont (Intcode (pc + 4) (mem // [(output, (m + n, a + b))]))
-        2 ->
-            let (m, a) = mem ! fst (mem ! (pc + 1))
-                (n, b) = mem ! fst (mem ! (pc + 2))
-                output = fst (mem ! (pc + 3))
-            in return $ Cont (Intcode (pc + 4) (mem // [(output, (m * n, a * b))]))
-        _ -> throwError "unknown opcode"
+  case opcode of
+    99 -> return Halt
+    1 ->
+      let (m, a) = mem ! fst (mem ! (pc + 1))
+          (n, b) = mem ! fst (mem ! (pc + 2))
+          output = fst (mem ! (pc + 3))
+       in return $ Cont (Intcode (pc + 4) (mem // [(output, (m + n, a + b))]))
+    2 ->
+      let (m, a) = mem ! fst (mem ! (pc + 1))
+          (n, b) = mem ! fst (mem ! (pc + 2))
+          output = fst (mem ! (pc + 3))
+       in return $ Cont (Intcode (pc + 4) (mem // [(output, (m * n, a * b))]))
+    _ -> throwError "unknown opcode"
   where
     opcode = fst (mem ! pc)
 
 run :: Bool -> Intcode -> Except String Intcode
 run trace p = do
-    s <- if trace then traceShow p (step p) else step p
-    
-    case s of
-      Halt -> return p
-      Cont p' -> run trace p'
+  s <- if trace then traceShow p (step p) else step p
+
+  case s of
+    Halt -> return p
+    Cont p' -> run trace p'
 
 main = do
-    input <- readFile "input.txt"
-    let program = map (\i -> (i, Lit i)) $ map read $ splitOn "," input
-        mem0 = listArray (0, length program - 1) program
-    
-    -- Part 1 & 2
-    let mem1 = mem0 // [(1, (12, Verb)), (2, (2, Noun))]
-        intcode = Intcode 0 mem1
-    case runExcept (run False intcode) of
-        Left err -> print $ "Error: " ++ err
-        Right (Intcode pc res) -> 
-            let (ans1, expr) = res ! 0
-                [ans2] = [ 100 * verb + noun
-                         | verb <- [0..99]
-                         , noun <- [0..99]
-                         , eval Env{..} expr == 19690720]
-            in do
-                putStrLn $ "Expr: " ++ show expr
-                putStrLn $ "Part 1: " ++ show ans1  -- 2692315
-                putStrLn $ "Part 2: " ++ show ans2  -- 9507
+  input <- readFile "input.txt"
+  let program = map (\i -> (i, Lit i)) $ map read $ splitOn "," input
+      mem0 = listArray (0, length program - 1) program
+
+  -- Part 1 & 2
+  let mem1 = mem0 // [(1, (12, Noun)), (2, (2, Verb))]
+      intcode = Intcode 0 mem1
+  case runExcept (run False intcode) of
+    Left err -> print $ "Error: " ++ err
+    Right (Intcode pc res) ->
+      let (ans1, expr) = res ! 0
+          [ans2] =
+            [ 100 * noun + verb
+            | verb <- [0 .. 99],
+              noun <- [0 .. 99],
+              eval Env {..} expr == 19690720
+            ]
+       in do
+            putStrLn $ "Expr: " ++ show expr
+            putStrLn $ "Part 1: " ++ show ans1 -- 2692315
+            putStrLn $ "Part 2: " ++ show ans2 -- 9507
 
