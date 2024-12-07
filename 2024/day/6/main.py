86e18a0c4d2217f9a0bbf2f6fd7084457a464591
liesen <johan@liesen.se>
2024-12-07 14:15:38 +0100

    2024-12-06

--- 2024/day/6/main.py
+++ 2024/day/6/main.py
@@ -1,5 +1,3 @@
-from itertools import product
-
 with open("input") as f:
     S = [ln.rstrip() for ln in f]
     numrows = len(S)
@@ -14,70 +12,41 @@ with open("input") as f:
 
 def walk(r: int, c: int, obstacles: set[tuple[int, int]]):
     dr, dc = -1, 0
-    seen: set[tuple[int, int]] = {(r, c)}
+    visited: set[tuple[int, int, int, int]] = {(r, c, dr, dc)}
 
     while 0 <= r + dr < numrows and 0 <= c + dc < numcols:
         if (r + dr, c + dc) in obstacles:
-            match (dr, dc):
-                case (-1, 0):
-                    dr, dc = 0, 1
-                case (1, 0):
-                    dr, dc = 0, -1
-                case (0, 1):
-                    dr, dc = 1, 0
-                case (0, -1):
-                    dr, dc = -1, 0
-
-            continue
-
-        r += dr
-        c += dc
-        seen.add((r, c))
-
-    return seen
-
-
-# Part 1
-path1 = walk(sr, sc, obstacles)
-print(len(path1))
-
-
-# Part 2
-def loop(r: int, c: int, obstacles: set[tuple[int, int]]):
-    dr, dc = -1, 0
-    seen2: set[tuple[int, int, int, int]] = {(r, c, dr, dc)}
-
-    while 0 <= r + dr < numrows and 0 <= c + dc < numcols:
-        if (r + dr, c + dc) in obstacles:
-            match (dr, dc):
-                case (-1, 0):
-                    dr, dc = 0, 1
-                case (1, 0):
-                    dr, dc = 0, -1
-                case (0, 1):
-                    dr, dc = 1, 0
-                case (0, -1):
-                    dr, dc = -1, 0
-                case _:
-                    pass
-
+            dr, dc = dc, -dr
         else:
             r += dr
             c += dc
 
-        if (r, c, dr, dc) in seen2:
-            return True
+        if (r, c, dr, dc) in visited:
+            return "loop", {(r, c) for r, c, _, _ in visited}
 
-        seen2.add((r, c, dr, dc))
+        visited.add((r, c, dr, dc))
 
-    return False
+    return "escape", {(r, c) for r, c, _, _ in visited}
+
+
+# Part 1
+match walk(sr, sc, obstacles):
+    case "escape", path1:
+        print(len(path1))
+
+    case _:
+        assert False, "loop in part 1"
+
+# Part 2
+ans2 = 0
 
 # Put an obstacle in the guards path to cause them to re-route
-ans2 = sum(
-    1
-    for r, c in path1
-    if not ((r, c) in obstacles or (r, c) == (sr, sc)) 
-    if loop(sr, sc, obstacles | {(r, c)})
-)
+for p in path1:
+    match walk(sr, sc, obstacles | {p}):
+        case "loop", _:
+            ans2 += 1
+
+        case _:
+            pass
 
 print(ans2)
