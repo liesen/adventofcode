a9ad1e1bddd84849dc9b5869131a0f415a8db7bc
liesen <johan@liesen.se>
2024-12-08 00:42:12 +0100

    2024-12-07 prune hopeless equations early

--- 2024/day/7/Main.hs
+++ 2024/day/7/Main.hs
@@ -3,16 +3,19 @@
 parseLine :: String -> (Int, [Int])
 parseLine (break (== ':') -> (ans, ':' : ' ' : terms)) = (read ans, map read (words terms))
 
-eval concat (x : xs) = foldl combine [x] xs
+valid part2 target [x] = target == x
+valid part2 target (x : xs) | target < x = False -- Prune continuations without a future (value of expression is strictly increasing)
+valid part2 target (x : y : ys) = any (\op -> valid part2 target (op x y : ys)) ops
   where
-    combine ys x = map (x +) ys ++ map (x *) ys ++ if concat then map (\y -> read (show y ++ show x)) ys else []
+    ops = [(+), (*)] ++ if part2 then [cat] else []
+    cat x y = read (show x ++ show y)
 
 main = do
   input <- readFile "input"
   let equations = map parseLine (lines input)
 
   -- Part 1
-  print $ sum [expected | (expected, terms) <- equations, expected `elem` eval False terms]
+  print $ sum [expected | (expected, terms) <- equations, valid False expected terms]
 
   -- Part 2
-  print $ sum [expected | (expected, terms) <- equations, expected `elem` eval True terms]
+  print $ sum [expected | (expected, terms) <- equations, valid True expected terms]
